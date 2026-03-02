5d2aaca0c120c9a5cf3672d83e9930fdff6ec67a
caruros <ccangana1@gmail.com>
2026-03-02 22:58:35 +0000

    home vainilla

---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Jumbotron = () => {
+    return (
+        <div className="p-5 mb-4 bg-light rounded-3">
+            <div className="container-fluid py-5">
+                <h1 className="display-5 fw-bold">A Warm Welcome!</h1>
+                <p className="col-md-8 fs-4">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+                <button className="btn btn-primary btn-lg">
+                    Call to action!
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Jumbotron;
