c80dd31d71fd4430b47b6db2d55d213d1a4c5acd
Isabella Escalona <isabellaescalona0110@gmail.com>
2026-03-02 19:36:15 -0400

    X

--- src/js/components/Jumbotron.jsx
+++ src/js/components/Jumbotron.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Jumbotron = () => {
     return (
-        <div className="p-5 mb-4 bg-light rounded-3 col-12">
+        <div className="p-5 mb-4 bg-light rounded-3">
             <div className="container-fluid py-5">
                 <h1 className="display-5 fw-bold">A Warm Welcome!</h1>
                 <p className="col-md-8 fs-4">
