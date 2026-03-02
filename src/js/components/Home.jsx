3faae962c1f72bae14dce221cb7206a050ccb889
Isabella Escalona <isabellaescalona0110@gmail.com>
2026-03-02 19:14:31 -0400

    Ajuste en el container del jumbotron y Cards

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -2,37 +2,24 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
+import Navbar from "./Navbar";
+import Jumbotron from "./Jumbotron";
+import Card from "./card";
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
-<<<<<<< HEAD
-
-
-			<div className="container mt-4">
+			<Navbar />
+			<div className="container col-md-6 col-sm-12 d-flex mt-4 mx-auto">
 				<Jumbotron />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
 			</div>
-		</>
-
-
-=======
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
 		</div>
->>>>>>> 5d2aaca0c120c9a5cf3672d83e9930fdff6ec67a
+
+
 	);
 };
 
